64c7978576db56fa2aadc3c0756806df2a40a517
faraz <farazdero2012@gmail.com>
2024-04-13 12:22:34 +0500

    hello

--- src/index.js
+++ src/index.js
@@ -9,27 +9,27 @@ import GamePage from "./components/GamePage";
 import CreateNewGame from "./components/CreateNewGame";
 import SearchResults from "./components/SearchResults";
 import {
-  Router,
-  BrowserRouter,
-  Route,
-  Routes,
-  Navigate,
+	Router,
+	BrowserRouter,
+	Route,
+	Routes,
+	Navigate,
 } from "react-router-dom";
 
 ReactDOM.render(
-  <div>
-    <BrowserRouter>
-      <Routes>
-        <Route path={"/home"} element={<App />} />
-        <Route path="/" element={<Navigate replace to="/home" />} />
-        <Route path={"/login"} element={<Login />} />
-        <Route path={"/signup"} element={<Signup />} />
-        <Route path="/game/:gameTitle" element={<GamePage />} />
-        <Route path="/createGame" element={<CreateNewGame />} />
-        <Route path="/search/:query" element={<SearchResults />} />
-        <Route path="/search" element={<App />} />
-      </Routes>
-    </BrowserRouter>
-  </div>,
-  document.getElementById("root")
+	<div>
+		<BrowserRouter>
+			<Routes>
+				<Route path={"/homee"} element={<App />} />
+				<Route path="/" element={<Navigate replace to="/homee" />} />
+				<Route path={"/login"} element={<Login />} />
+				<Route path={"/signup"} element={<Signup />} />
+				<Route path="/game/:gameTitle" element={<GamePage />} />
+				<Route path="/createGame" element={<CreateNewGame />} />
+				<Route path="/search/:query" element={<SearchResults />} />
+				<Route path="/search" element={<App />} />
+			</Routes>
+		</BrowserRouter>
+	</div>,
+	document.getElementById("root")
 );
